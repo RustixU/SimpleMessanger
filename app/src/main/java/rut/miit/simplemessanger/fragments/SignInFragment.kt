f5a5b0ee1c0d5351a560bb8ed50197c3c0ec8fba
Rustam <avezov@rustam-sh.ru>
2024-11-16 17:33:37 +0300

    Очистил код от лишнего

--- app/src/main/java/rut/miit/simplemessanger/fragments/SignInFragment.kt
+++ app/src/main/java/rut/miit/simplemessanger/fragments/SignInFragment.kt
@@ -2,19 +2,16 @@ package rut.miit.simplemessanger.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import rut.miit.simplemessanger.MainActivity
 import rut.miit.simplemessanger.R
 import rut.miit.simplemessanger.databinding.FragmentSignInBinding
-import rut.miit.simplemessanger.databinding.FragmentSignUpBinding
 
 private const val ARG_USERNAME = "username"
 private const val ARG_PASSWORD = "password"
