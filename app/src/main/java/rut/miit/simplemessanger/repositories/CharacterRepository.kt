a8467f1dd1042f7fc750185cac9a41fc9d55518b
Rustam <avezov@rustam-sh.ru>
2024-12-12 18:48:49 +0300

    обработка ошибок

--- app/src/main/java/rut/miit/simplemessanger/repositories/CharacterRepository.kt
+++ app/src/main/java/rut/miit/simplemessanger/repositories/CharacterRepository.kt
@@ -1,18 +1,36 @@
 package rut.miit.simplemessanger.repositories
 
+import android.database.SQLException
+import android.util.Log
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import rut.miit.simplemessanger.dao.CharacterDao
 import rut.miit.simplemessanger.entity.Character
 
 class CharacterRepository(private val dao: CharacterDao) {
 
-    fun getCharacters(): Flow<List<Character>> = dao.getAll()
+    fun getCharacters(): Flow<List<Character>> {
+        try {
+            return dao.getAll()
+        } catch (ex: NoSuchElementException) {
+            Log.d("Error SQL", "No such elements")
+            return emptyFlow()
+        }
+    }
 
     suspend fun insertCharacters(characters: List<Character>) {
-        dao.insert(characters)
+        try {
+            dao.insert(characters)
+        } catch (ex: SQLException) {
+            Log.d("Error SQL", "vThe SQL query could not be executed")
+        }
     }
 
     fun deleteAll() {
-        dao.deleteAll()
+        try {
+            dao.deleteAll()
+        } catch (ex: SQLException) {
+            Log.d("Error SQL", "The SQL query could not be executed")
+        }
     }
 }
